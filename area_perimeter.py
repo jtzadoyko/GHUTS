ce85467b23e296cc7f01d13ee5fd4292b8bc2df7
zuds <44326766+jtzadoyko@users.noreply.github.com>
2020-10-29 13:28:27 -0400

    Create area_perimeter.py

---
+++ area_perimeter.py
@@ -0,0 +1,14 @@
+#TODO: Edit the get_area_and_perimeter function to return an
+# area and perimeter, once you have completed this by running the application
+# the run_test_case will execute, if the program does not fail, that means your
+# code has succesfully ran and you have completed this exercise
+
+def get_area_and_perimeter(height, width):
+  pass
+
+def run_test_case():
+  area, perimeter = get_area_and_perimeter(10,10)
+  assert area == 100
+  assert perimeter == 40
+
+run_test_cases()
